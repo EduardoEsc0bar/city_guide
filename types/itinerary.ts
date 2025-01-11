734d86e5fb79fbb4219baf822793db800f65947a
EduardoEsc0bar <158665573+EduardoEsc0bar@users.noreply.github.com>
2025-01-11 04:34:48 -0500

    Update itinerary.ts

--- types/itinerary.ts
+++ types/itinerary.ts
@@ -1,44 +1,36 @@
 export interface ItineraryActivity {
-    name: string;
-    time: string;
-    duration: number;
-    description: string;
-    transportation?: string;
-    address?: string;
-  }
-  
-  export interface ItinerarySection {
-    title: string;
-    activities: ItineraryActivity[];
-  }
-  
-  export interface ItineraryDay {
-    dayNumber: number;
-    sections: ItinerarySection[];
-  }
-  
-  export interface Restaurant {
-    name: string;
-    address: string;
-    rating: number;
-  }
-  
-  export interface Accommodation {
-    name: string;
-    address: string;
-    rating: number;
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  name: string;
+  time: string;
+  duration: number;
+  description: string;
+  transportation?: string;
+  address?: string;
+}
+
+export interface ItinerarySection {
+  title: string;
+  activities: ItineraryActivity[];
+}
+
+export interface ItineraryDay {
+  dayNumber: number;
+  sections: ItinerarySection[];
+}
+
+export interface Restaurant {
+  name: string;
+  address: string;
+  rating: number;
+}
+
+export interface Accommodation {
+  name: string;
+  address: string;
+  rating: number;
+}
+
+export interface Location {
+  name: string;
+  address: string;
+}
+
